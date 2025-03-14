1bc767ae40a5c19ef0b0ec0b3b7087ad5dadcc1a
Gabby <gabbzf@gmail.com>
2025-03-14 10:11:55 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CodeTestFriday
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
