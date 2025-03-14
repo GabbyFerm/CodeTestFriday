13a3a837a9431b3172a406d6d81550c71d7cc347
Gabby <gabbzf@gmail.com>
2025-03-14 10:46:53 +0100

    Method to group anagrams

---
+++ AnagramGroup.cs
@@ -0,0 +1,35 @@
+﻿namespace CodeTestFriday
+{
+    public class AnagramGroup
+    {
+
+        public static List<List<string>> GroupAnagrams(List<string> wordList)
+        {
+
+            // Skapa en dictionary för att hålla koll på anagrammen
+            Dictionary<string, List<string>> anagramGroups = new Dictionary<string, List<string>>();
+
+            // Gå igenom varje ord i listan
+            foreach (var word in wordList)
+            {
+                // Sortera bokstäverna i ordet
+                var sortedWord = new string(word.OrderBy(character => character).ToArray());
+
+                // Om sorterade ordet redan finns i dictionary
+                if (anagramGroups.ContainsKey(sortedWord))
+                {
+                    // Lägg till ordet i listan för detta anagram
+                    anagramGroups[sortedWord].Add(word);
+                }
+                else
+                {
+                    // Skapa en ny lista för detta anagram
+                    anagramGroups[sortedWord] = new List<string> { word };
+                }
+            }
+
+            // Returnera alla anagramgrupper
+            return anagramGroups.Values.ToList();
+        }
+    }
+}
